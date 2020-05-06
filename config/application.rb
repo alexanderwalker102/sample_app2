53ec1ae7bb72e3ae0827ac789e45d249091cb021
AlexW <avasilev.hse@gmail.com>
2020-05-06 14:25:38 +0000

    gravatar done

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SampleApp2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.web_console.whitelisted_ips = '87.240.46.149'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
   
   test "should be valid" do
@@ -53,6 +53,11 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase 
     @user.save
     assert_not duplicate_user.valid?
+  end 
+  
+   test "password length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end 
   # test "the truth" do
   #   assert true
